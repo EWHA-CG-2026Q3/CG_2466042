053e97e2d72fd3c6c9b211857c58eed87067a490
wvxv <qawsedrftgy454@gmail.com>
2026-09-19 19:45:21 +0900

    S04 다이아몬드 기본 형태 구현

--- Assets/S04/S04_Diamond_Script.cs
+++ Assets/S04/S04_Diamond_Script.cs
@@ -5,21 +5,29 @@ public class S04_Diamond_Script : MonoBehaviour
 {
     void Start()
     {
-        // 정육면체 8개 정점(S3) 중 0, 2, 5, 7번 — 대각선으로 고르면 정사면체가 됨
         Vector3[] vertices = new Vector3[]
         {
             new Vector3(0f, 0f, 0f), // 0 (정육면체 0번)
-            new Vector3(1f, 1f, 0f), // 1 (정육면체 2번)
+            new Vector3(1f, 0f, 0f), // 1 (정육면체 1번)
             new Vector3(1f, 0f, 1f), // 2 (정육면체 5번)
-            new Vector3(0f, 1f, 1f), // 3 (정육면체 7번)
+            new Vector3(0f, 0f, 1f), // 3 (정육면체 4번)
+            new Vector3(0.5f, 1f, 0.5f), //4
+            new Vector3(0.5f, -1f, 0.5f), //5
+         
         };
 
         int[] triangles = new int[]
         {
-            1, 3, 2,  // 밑면
-            0, 1, 2,  // 옆면 A
-            0, 2, 3,  // 옆면 B
-            0, 3, 1,  // 옆면 C
+            0, 1, 4,
+            1, 2, 4, 
+            2, 3, 4,
+            0, 3, 4,
+
+            0, 1, 5,
+            1, 2, 5, 
+            2, 3, 5, 
+            3, 0, 5,
+
         };
 
         // 잘못된 예시 — 모든 삼각형의 정점 순서가 반대로 뒤집혀 있음
